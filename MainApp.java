68ae9b5081c5af278b311a6e06208b7b1267c262
ThodTsi <teotsi4002@gmail.com>
2023-05-26 21:19:28 +0300

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -4,11 +4,24 @@ class mainApp{
 
     public static void main(String Args[]){
 
-
+    Scanner in = new Scanner(System.in);
     HashMap <Integer,CommercialCompany> company = new HashMap <Integer,CommercialCompany>;
     HashMap <Integer,Product> products = new HashMap <Integer,Product>;
     HashMap <Integer,AdType> types = new HashMap <Integer,AdType>;
     HashMap <Integer,Ad> adverts = new HashMap <Integer,Ad>;
-   
+    int option = in.nextInt();
+    switch(option){
+        
+    }
+
+
+
+
+
+
+
+
+
+
     }
 }
