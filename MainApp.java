1d6203b5bb40ba2fee12bd075079a2cc59e5b5c4
ThodTsi <teotsi4002@gmail.com>
2023-05-27 14:44:28 +0300

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -10,6 +10,15 @@ class mainApp{
     HashMap <Integer,AdType> types = new HashMap <Integer,AdType>();
     HashMap <Integer,Ad> adverts = new HashMap <Integer,Ad>();
     int option = in.nextInt();
+    System.out.println("1. Insert new company");
+    System.out.println("2. Insert new ad type");
+    System.out.println("3. Insert new ad");
+    System.out.println("4. Show ads");
+    System.out.println("5. Show company's ads");
+    System.out.println("6. Calculate company's cost");
+    System.out.println("7. Number of products' ads");
+    System.out.println("8. Calculate cost of a products' ad");
+    System.out.println("9. Show ad cost per product");
     switch(option){
         
     }
