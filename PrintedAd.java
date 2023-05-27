f31ac4c2dea7dcfb710c3c2cbf645eb0478427bf
ThodTsi <teotsi4002@gmail.com>
2023-05-27 14:33:57 +0300

    Update PrintedAd.java

--- PrintedAd.java
+++ PrintedAd.java
@@ -1,6 +1,6 @@
 class PrintedAd extends Ad{
     private int words, position;
-    Ad(int code_type, int code_prod, int duration, String detail, int words, int position){
+    PrintedAd(int code_type, int code_prod, int duration, int detail, int words, int position){
         super(code_type,code_prod,duration,detail);
         this.words = words;
         this.position = position;
