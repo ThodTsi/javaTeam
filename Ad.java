c3399fe3ef1652e067a1f01288f4764d3bf7ed44
ThodTsi <teotsi4002@gmail.com>
2023-05-27 14:33:37 +0300

    Update Ad.java

--- Ad.java
+++ Ad.java
@@ -1,3 +1,9 @@
 public class Ad{
     private int code_type, code_prod, duration, detail;
-    
+    Ad(int code_type, int code_prod, int duration, int detail){
+        this.code_type = code_type;
+        this.code_prod = code_prod;
+        this.duration = duration;
+        this.detail = detail;
+    }
+}
