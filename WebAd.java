973414c21d60a80029b4654f3d749462ff51cd6f
ThodTsi <teotsi4002@gmail.com>
2023-05-28 15:54:16 +0300

    Update WebAd.java

--- WebAd.java
+++ WebAd.java
@@ -1,7 +1,7 @@
 class WebAd extends Ad{
     private int autoshow, extrap;
-    WebAd(int code_type, int code_prod, int duration, int detail, int autoshow, int extrap){
-        super(code_type,code_prod,duration,detail);
+    WebAd(int code_type, int code_prod, int duration, int details, int autoshow, int extrap){
+        super(code_type,code_prod,duration,details);
         this.autoshow = autoshow;
         this.extrap = extrap;
     }
