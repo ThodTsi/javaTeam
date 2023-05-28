408e3089dd27424afa05848708b606ec0d784008
Styliani04 <moumtzistella0@gmail.com>
2023-05-28 16:29:18 +0300

    Update DigitalAd.java

--- DigitalAd.java
+++ DigitalAd.java
@@ -1,6 +1,7 @@
 class DigitalAd extends Ad{
-    private int dur_sec, timezone;
-    DigitalAd(int code_type, int code_prod, int duration, int details, int dur_sec, int timezone){
+    private int dur_sec;
+    private String timezone;
+    DigitalAd(int code_type, int code_prod, int duration, int details, int dur_sec, String timezone){
         super(code_type,code_prod,duration,details);
         this.dur_sec = dur_sec;
         this.timezone = timezone;
@@ -9,8 +10,13 @@ class DigitalAd extends Ad{
     public String toString(){
         return super.toString() + "/n" + "Duration in seconds: " + dur_sec + "/n" + "Timezone: " + timezone ;
     }
+    
+    public int getDur(){
+        return dur_sec;
+    }
 
-
-
+    public String TimeZone(){
+        return timezone;
+    }
 
 }
