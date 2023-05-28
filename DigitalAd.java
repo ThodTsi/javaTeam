fc7f436af3e67440e6472138a1a6153eb78ad360
ThodTsi <teotsi4002@gmail.com>
2023-05-28 15:54:06 +0300

    Update DigitalAd.java

--- DigitalAd.java
+++ DigitalAd.java
@@ -1,7 +1,7 @@
 class DigitalAd extends Ad{
     private int dur_sec, timezone;
-    DigitalAd(int code_type, int code_prod, int duration, int detail, int dur_sec, int timezone){
-        super(code_type,code_prod,duration,detail);
+    DigitalAd(int code_type, int code_prod, int duration, int details, int dur_sec, int timezone){
+        super(code_type,code_prod,duration,details);
         this.dur_sec = dur_sec;
         this.timezone = timezone;
     }
