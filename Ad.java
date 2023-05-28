9257a2cb92e917ae0ab1b2d0aa3297b01115a297
ThodTsi <teotsi4002@gmail.com>
2023-05-28 15:54:03 +0300

    Update Ad.java

--- Ad.java
+++ Ad.java
@@ -1,14 +1,14 @@
 public class Ad{
-    private int code_type, code_prod, duration, detail;
-    Ad(int code_type, int code_prod, int duration, int detail){
+    private int code_type, code_prod, duration, details;
+    Ad(int code_type, int code_prod, int duration, int details){
         this.code_type = code_type;
         this.code_prod = code_prod;
         this.duration = duration;
-        this.detail = detail;
+        this.details = details;
     }
 
     public String toString(){
-        return "Code of type: " + code_type + "/n" + "Code of product: " + code_prod + "/n" + "Duration in days: " + duration + "/n" + "Details: " + detail; 
+        return "Code of type: " + code_type + "/n" + "Code of product: " + code_prod + "/n" + "Duration in days: " + duration + "/n" + "Details: " + details; 
     }
 
 
