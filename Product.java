dbbaa3a631bb397f182c995acee9f5b04e09d125
ThodTsi <teotsi4002@gmail.com>
2023-05-28 11:34:55 +0300

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,4 +1,10 @@
 public class Product{
-    private int code,afm;
-    private String desc;
+    private int code;
+    private String afm,description;
+    Product(int code, String afm, String description){
+        this.code = code;
+        this.afm = afm;
+        this.description;
+
+    }
 }
