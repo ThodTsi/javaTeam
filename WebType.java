e055c52b4ec976e7be83333ebb2fb4ab7bec7e0b
ThodTsi <teotsi4002@gmail.com>
2023-05-29 18:00:00 +0300

    Update WebType.java

--- WebType.java
+++ WebType.java
@@ -7,6 +7,14 @@ public class WebType extends AdType{
         this.extrap_cost = extrap_cost;
     }
 
+    public int cost(int autoshow, int extrapages, int duration){
+        if(autoshow == 1){
+            return ppd * duration + cost_auto + extrapages * extrap_cost;
+        }else{
+            return ppd * duration + extrapages * extrap_cost;
+        }
+    }
+
     
 
 
