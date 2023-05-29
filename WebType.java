b3a898369e44fb9a4b6fc8af17a8f2167f1a393c
ThodTsi <teotsi4002@gmail.com>
2023-05-29 17:00:40 +0300

    Update WebType.java

--- WebType.java
+++ WebType.java
@@ -1,6 +1,6 @@
 public class WebType extends AdType{
     private int ppd, cost_auto, extrap_cost;
-    WebType(int code, String description, int afm, int ppd, int cost_auto, int extrap_cost){
+    WebType(int code, String description, String afm, int ppd, int cost_auto, int extrap_cost){
         super(code,description,afm);
         this.ppd = ppd;
         this.cost_auto = cost_auto;
