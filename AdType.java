3cd2eeedf0e339ddbba1584ea05e39f431ff748d
ThodTsi <teotsi4002@gmail.com>
2023-05-29 17:00:19 +0300

    Update AdType.java

--- AdType.java
+++ AdType.java
@@ -1,11 +1,20 @@
 public class AdType {
 
-    private int code,afm;
-    private String description;
-    AdType(int code,String description, int afm){
+    private int code;
+    private String description,afm;
+    AdType(int code,String description, String afm){
         this.code = code;
         this.description = description;
         this.afm = afm;
     }
+
+    public String getAfm(){
+        return this.afm;
+    }
+
+    public int getCode(){
+        return this.code;
+    }
+    
 
 }
