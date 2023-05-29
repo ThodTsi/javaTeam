771df2b88fe5e75b50b0ffdd2d8227fad233ebf7
ThodTsi <teotsi4002@gmail.com>
2023-05-29 17:00:37 +0300

    Update PrintedType.java

--- PrintedType.java
+++ PrintedType.java
@@ -1,7 +1,7 @@
 public class PrintedType extends AdType{
 
     private int ppwFirst, ppwMid, ppwLast;
-    PrintedType(int code, String description, int afm, int ppwFirst, int ppwMid, int ppwLast){
+    PrintedType(int code, String description, String afm, int ppwFirst, int ppwMid, int ppwLast){
         super(code,description,afm);
         this.ppwFirst = ppwFirst;
         this.ppwMid = ppwMid;
