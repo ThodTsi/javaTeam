4bf0c9f02b4e241e5458dfbe4455a43d8228f815
Styliani04 <moumtzistella0@gmail.com>
2023-06-13 19:25:25 +0300

    ena akoma commit

--- WebType.java
+++ WebType.java
@@ -17,4 +17,8 @@ public class WebType extends AdType{
         }
     }
 
+    public String toStringFile(){
+        return super.toStringFile() + "\n\t\tPRICE_PER_DAY " + ppd + "\n\t\tAUTOSHOW_COST " + cost_auto + "\n\t\tEXTRA_PAGES_COST " + extrap_cost + "\n\t\tTYPE Web\n\t}";
+    }
+
 }
