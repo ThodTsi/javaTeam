d251c1ca1fefa56ca551227477f2cb4cd27b03f4
Styliani04 <moumtzistella0@gmail.com>
2023-06-13 19:35:41 +0300

    ela efyges

--- CommercialCompany.java
+++ CommercialCompany.java
@@ -19,19 +19,6 @@ public class CommercialCompany{
         return this.name;
     }
 
-    private void createFile(String, ArrayList<String> CommercialCompany){
-        throws IOException {
-            FileWriter writer = new FileWriter(file + ".txt");
-            int size = CommercialCompany.size()
-            for (int i = 0;i<size;i++){
-                writer.write("AFM: " + CommercialCompany.getAfm() + "\nName: " + CommercialCompany.getName());
-            } 
-            writer.close();
-
-        }
-        
-    }
-
     public String toStringFile(){
         return "\n\tCOMPANY\n\t{\n\t\tNAME " + name + "\n\t\tAFM " + afm ;
     }
